2d5122059322020fac3c89be4435e97702de6690
Komoini <komolafeinioluwa9@gmail.com>
2023-04-25 00:28:40 +0100

    further commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-
-import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+import Search from './Search';
+import './index.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const handleSubmit = async (e) => {
+    const config = { params: { q: searchTerm } };
+     const response = await axios.get(`https://api.tvmaze.com/search/shows?`, config);
+     console.log(response.data)
+    setSearchTerm('')
+  }
+
   return (
-    <div className="App">
-    <h1>Hello world</h1>
+    <div className="text-center pt-8">
+    <h1 className='font-bold text-3xl my-8 pb-4 font-sans text-gray-500'>DNB's Movie-search App</h1>
+    <div>
+    <input
+    className="input-field bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500  w-9/12 p-2.5"
+    type="text"
+    value={searchTerm}
+    placeholder="Search Movies here.."
+    onChange={(e) => setSearchTerm(e.target.value)}
+  />
+  <button
+    className="text-white bg-blue-700 hover:bg-blue-800  focus:outline-none  font-medium  text-sm  sm:w-auto px-5 py-2.5 text-center"
+    onClick={() => handleSubmit()}
+  >
+    Search
+  </button>
+    </div>
+    <Search/>
     </div>
   );
 }
